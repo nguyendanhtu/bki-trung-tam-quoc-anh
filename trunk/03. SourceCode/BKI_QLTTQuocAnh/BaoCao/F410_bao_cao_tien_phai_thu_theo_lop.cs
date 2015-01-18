9dc5a248ea1e4afe30df42274ef8deef2d38305c
huynhdaihai@gmail.com <huynhdaihai@gmail.com@400e2d81-3ff3-c16d-9fe2-d9ee859fe9cb>
2015-01-18 04:10:52 +0000

    Design form bao cao f410

---
+++ trunk/03. SourceCode/BKI_QLTTQuocAnh/BaoCao/F410_bao_cao_tien_phai_thu_theo_lop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BKI_QLTTQuocAnh.BaoCao
+{
+    public partial class F410_bao_cao_tien_phai_thu_theo_lop : Form
+    {
+        public F410_bao_cao_tien_phai_thu_theo_lop()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
